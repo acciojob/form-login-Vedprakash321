c2266b53323941fcaca3d8ff8cc20f76bac6f224
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-09-28 16:26:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function showAlert() {
+function  getFormvalue() {
 	const firstName = document.getElementById("firstName").value;
     const lastName = document.getElementById("lastName").value;
-	alert(`${firstName} ${lastName}`);
+	alert(`${firstName}  ${lastName}`);
 }
