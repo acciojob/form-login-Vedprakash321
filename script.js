750b09f913b3984aee4e0d8826fc96e4e5cbff64
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-09-28 18:42:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function  getFormvalue() {
 	const firstName = document.getElementById("firstName").value;
     const lastName = document.getElementById("lastName").value;
-	alert(`${firstName}  ${lastName}`);
+	alert(`${firstName}${lastName}`);
 }
